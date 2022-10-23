da6a21dc8bd0babcf654a505d3d43c2c17c5e183
simplysabir <simplysabir@gmail.com>
2022-10-23 12:53:41 +0530

    Logic Reamining

---
+++ index.js
@@ -0,0 +1,7 @@
+/* LOGIC EXPLAINED */
+
+Create a Math Question
+Math Question will have a random Generated
+Question Type Multiplication Question with Random Generated ANswer
+if answer will be correct then score will be incremented
+if answer will be wrong then score will be decremented
